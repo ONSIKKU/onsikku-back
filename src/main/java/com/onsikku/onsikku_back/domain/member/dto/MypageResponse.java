082ec08543c328d8173c1008ff150ccb8cc98658
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-08-17 18:57:25 +0900

    feat : 멤버에 알림 수신 여부 칼럼 추가

--- src/main/java/com/onsikku/onsikku_back/domain/member/dto/MypageResponse.java
+++ src/main/java/com/onsikku/onsikku_back/domain/member/dto/MypageResponse.java
@@ -5,8 +5,6 @@ import com.onsikku.onsikku_back.domain.member.domain.FamilyRole;
 import com.onsikku.onsikku_back.domain.member.domain.Gender;
 import com.onsikku.onsikku_back.domain.member.domain.Member;
 import com.onsikku.onsikku_back.domain.member.domain.Role;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,30 +18,25 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MypageResponse {
-  private UUID id;
-  private UUID familyId;
   private String familyName;
   private String familyInvitationCode;
-  @Enumerated(EnumType.STRING)
   private Role role;
-  private String kakaoId;
   private String profileImageUrl;
   private FamilyRole familyRole;
   private LocalDate birthDate;
   private Gender gender;
+  private boolean isAlarmEnabled;
 
   public static MypageResponse from(Member member) {
     return MypageResponse.builder()
-        .id(member.getId())
-        .familyId(member.getFamily().getId())
         .familyName(member.getFamily().getFamilyName())
         .familyInvitationCode(member.getFamily().getInvitationCode())
         .role(member.getRole())
-        .kakaoId(member.getKakaoId())
         .profileImageUrl(member.getProfileImageUrl())
         .familyRole(member.getFamilyRole())
         .birthDate(member.getBirthDate())
         .gender(member.getGender())
+        .isAlarmEnabled(member.isAlarmEnabled())
         .build();
   }
 }
