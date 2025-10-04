10f4112c10a2898cf9ca6b0deea4534037f2fc93
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-10-04 21:35:30 +0900

    refactor : 디렉토리 이름 변경 entity -> domain

--- src/main/java/com/onsikku/onsikku_back/domain/ai/repository/AnswerAnalysisRepository.java
+++ src/main/java/com/onsikku/onsikku_back/domain/ai/repository/AnswerAnalysisRepository.java
@@ -1,6 +1,6 @@
 package com.onsikku.onsikku_back.domain.ai.repository;
 
-import com.onsikku.onsikku_back.domain.ai.entity.AnswerAnalysis;
+import com.onsikku.onsikku_back.domain.ai.domain.AnswerAnalysis;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
