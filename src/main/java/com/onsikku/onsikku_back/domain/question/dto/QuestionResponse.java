e33f05b151899647e2f53a7cfaff80fc258717ec
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-11-23 02:52:06 +0900

    refactor : 필드명 변경

--- src/main/java/com/onsikku/onsikku_back/domain/question/dto/QuestionResponse.java
+++ src/main/java/com/onsikku/onsikku_back/domain/question/dto/QuestionResponse.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
@@ -16,5 +17,6 @@ public class QuestionResponse {
   private int totalQuestions;
   private int answeredQuestions;
   private int totalReactions;
-  private List<QuestionAssignment> todayAssignments;
+  private List<QuestionAssignment> todayQuestionAssignments;
+  private UUID todayQuestionInstanceId;
 }
