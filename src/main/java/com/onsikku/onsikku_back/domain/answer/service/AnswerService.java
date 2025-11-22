05814c85c84ce4eca2613946723f128c72e2a27f
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-11-23 02:54:13 +0900

    fix : 답변 로직 개선

--- src/main/java/com/onsikku/onsikku_back/domain/answer/service/AnswerService.java
+++ src/main/java/com/onsikku/onsikku_back/domain/answer/service/AnswerService.java
@@ -4,6 +4,7 @@ package com.onsikku.onsikku_back.domain.answer.service;
 import com.onsikku.onsikku_back.domain.ai.service.AiRequestService;
 import com.onsikku.onsikku_back.domain.answer.dto.AnswerRequest;
 import com.onsikku.onsikku_back.domain.answer.domain.Answer;
+import com.onsikku.onsikku_back.domain.answer.dto.AnswerResponse;
 import com.onsikku.onsikku_back.domain.answer.repository.AnswerRepository;
 import com.onsikku.onsikku_back.domain.member.domain.Member;
 import com.onsikku.onsikku_back.domain.question.domain.QuestionAssignment;
@@ -24,34 +25,35 @@ public class AnswerService {
     private final QuestionAssignmentRepository questionAssignmentRepository;
     private final AiRequestService aiRequestService;
 
-    public List<Answer> findAnswer(UUID questionAssignmentId, Member member) {
-        findQuestionAssignment(questionAssignmentId, member);
-        return answerRepository.findByQuestionAssignmentId(questionAssignmentId);
-    }
-
-    // TODO : 답변 생성 후 답변 분석 요청 타이밍 고려
     @Transactional
-    public Answer createAnswer(AnswerRequest request, Member member) {
-        QuestionAssignment assignment = findQuestionAssignment(request.questionAssignmentId(), member);
+    public AnswerResponse createAnswer(AnswerRequest request, Member member) {
+        QuestionAssignment assignment = findQuestionAssignmentAndAuthorizeFamily(request.questionAssignmentId(), member);
         if (!assignment.getMember().getId().equals(member.getId())) {
             throw new BaseException(BaseResponseStatus.CANNOT_ANSWER_OTHER_QUESTION);
+        }
+        // 중복 답변 확인
+        List<Answer> answers = answerRepository.findByQuestionAssignmentId(request.questionAssignmentId());
+        for(Answer answer : answers) {
+            if(answer.getMember().getId().equals(member.getId())) {
+                throw new BaseException(BaseResponseStatus.ALREADY_ANSWERED_QUESTION);
+            }
         }
         Answer newAnswer = Answer.create(assignment, member, request.answerType(), request.content());
         assignment.markAsAnswered();
         // AI 분석 요청
-        // aiRequestService.analyzeAnswer(newAnswer, request 만들기..);
-        return answerRepository.save(newAnswer);
+        //aiRequestService.analyzeAnswer(newAnswer, answer);
+        return AnswerResponse.from(answerRepository.save(newAnswer));
     }
 
     @Transactional  // @Transactional에 의해 변경 감지(dirty checking)가 동작하므로 save를 호출할 필요가 없음
-    public Answer updateAnswer(AnswerRequest request, Member member) {
-        Answer answer = answerRepository.findById(request.id())
+    public AnswerResponse updateAnswer(AnswerRequest request, Member member) {
+        Answer answer = answerRepository.findById(request.answerId())
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.ANSWER_NOT_FOUND));
         if (!answer.getMember().getId().equals(member.getId())) {
             throw new BaseException(BaseResponseStatus.CANNOT_ANSWER_OTHER_QUESTION);
         }
         answer.updateContent(request.content());
-        return answer;
+        return AnswerResponse.from(answerRepository.save(answer));
     }
 
     // ---------------------------- 테스트용 ----------------------------
@@ -62,7 +64,7 @@ public class AnswerService {
         answerRepository.delete(answer);
     }
 
-    private QuestionAssignment findQuestionAssignment(UUID questionAssignmentId, Member member) {
+    private QuestionAssignment findQuestionAssignmentAndAuthorizeFamily(UUID questionAssignmentId, Member member) {
         QuestionAssignment assignment = questionAssignmentRepository.findById(questionAssignmentId)
             .orElseThrow(() -> new BaseException(BaseResponseStatus.QUESTION_ASSIGNMENT_NOT_FOUND));
         if(!assignment.getMember().getFamily().getId().equals(member.getFamily().getId())) {
