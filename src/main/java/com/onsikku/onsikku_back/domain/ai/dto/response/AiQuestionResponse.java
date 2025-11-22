0e52f793758a7e5531f349540063442dec55e071
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-11-23 02:48:11 +0900

    fix : 카멜 케이스로 변경

--- src/main/java/com/onsikku/onsikku_back/domain/ai/dto/response/AiQuestionResponse.java
+++ src/main/java/com/onsikku/onsikku_back/domain/ai/dto/response/AiQuestionResponse.java
@@ -24,21 +24,12 @@ public class AiQuestionResponse {
   private UUID usedTemplateId; // 사용된 템플릿 ID, setter로 설정 가능
 
   private String content;
-  @JsonProperty("planned_date")
-  private LocalDate plannedDate;
   private String status;
-  @JsonProperty("generated_by")
   private String generatedBy;
-  @JsonProperty("generation_model")
   private String generationModel;
-  @JsonProperty("generation_parameters")
   private JsonNode generationParameters; // {}는 특정 타입이 없으므로 Object 또는 Map<String, Object>로 받음
-  @JsonProperty("generation_prompt")
   private String generationPrompt;
-  @JsonProperty("generation_metadata")
   private JsonNode generationMetadata;
-  @JsonProperty("generation_confidence")
   private float generationConfidence;
-  @JsonProperty("generated_at")
   private LocalDateTime generatedAt;
 }
