4805c3989f01ec85a936faf3bd4e031a868ebf6f
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-11-27 11:04:52 +0900

    feat : 답변 분석 관련 리포지토리 코드 추가

--- src/main/java/com/onsikku/onsikku_back/domain/ai/repository/AnswerAnalysisRepository.java
+++ src/main/java/com/onsikku/onsikku_back/domain/ai/repository/AnswerAnalysisRepository.java
@@ -1,10 +1,16 @@
 package com.onsikku.onsikku_back.domain.ai.repository;
 
 import com.onsikku.onsikku_back.domain.ai.domain.AnswerAnalysis;
+import com.onsikku.onsikku_back.domain.answer.domain.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AnswerAnalysisRepository extends JpaRepository<AnswerAnalysis, UUID> {
 
+  @Query("SELECT aa FROM AnswerAnalysis aa JOIN FETCH aa.answer a WHERE a.member.id = :memberId")
+  List<AnswerAnalysis> findAllAnalysesByMemberId(@Param("memberId") UUID memberId);
 }
