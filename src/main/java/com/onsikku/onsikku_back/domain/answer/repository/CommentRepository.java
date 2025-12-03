c4fa6a5c34f6827d930b65391fcdddafe7f05415
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-12-03 19:47:07 +0900

    fix : 대댓글 직렬화 오류 수정

--- src/main/java/com/onsikku/onsikku_back/domain/answer/repository/CommentRepository.java
+++ src/main/java/com/onsikku/onsikku_back/domain/answer/repository/CommentRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
-  @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.parent p JOIN FETCH c.member " +
+  @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.parent p JOIN FETCH c.member LEFT JOIN FETCH p.member " +
       "WHERE c.questionInstance.id = :instanceId " +
       "ORDER BY c.createdAt DESC")
   List<Comment> findAllByQuestionInstanceIdWithParentOrderByCreatedAtDesc(@Param("instanceId") UUID questionInstanceId);
