f27d842acf9aa1cefc244e2a4540e9db82dd3710
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-12-04 16:58:50 +0900

    chores : 로깅 관련 처리

--- src/main/java/com/onsikku/onsikku_back/OnsikkuBackApplication.java
+++ src/main/java/com/onsikku/onsikku_back/OnsikkuBackApplication.java
@@ -1,9 +1,12 @@
 package com.onsikku.onsikku_back;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class OnsikkuBackApplication {
@@ -11,5 +14,9 @@ public class OnsikkuBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnsikkuBackApplication.class, args);
 	}
-
+	@PostConstruct
+	public void init() {
+		// JVM의 기본 시간대를 'Asia/Seoul'로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
