d300bb9df8f90e1d4df74c6c6dc64184979e0d54
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2026-02-04 23:57:57 +0900

    Update AuthResponse.java

--- src/main/java/com/onsikku/onsikku_back/global/auth/dto/AuthResponse.java
+++ src/main/java/com/onsikku/onsikku_back/global/auth/dto/AuthResponse.java
@@ -1,5 +1,6 @@
 package com.onsikku.onsikku_back.global.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthResponse {
   private boolean isRegistered;
   private String accessToken;
