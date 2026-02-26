b2fe13978a42e53ccebbff9ec84f8a6cf4ae8b37
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2026-02-26 19:07:54 +0900

    Update OnsikkuBackApplication.java

--- src/main/java/com/onsikku/onsikku_back/OnsikkuBackApplication.java
+++ src/main/java/com/onsikku/onsikku_back/OnsikkuBackApplication.java
@@ -1,12 +1,7 @@
 package com.onsikku.onsikku_back;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class OnsikkuBackApplication {
